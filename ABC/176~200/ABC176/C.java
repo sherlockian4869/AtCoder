f274ea6fb6d3bef8b776cf4b409aea50935353bf
sherlockian4869 <k.yaeo@carep-tech.com>
2021-09-09 16:37:06 +0900

    ファイルの移動

--- ABC/176~200/ABC176/C.java
+++ ABC/176~200/ABC176/C.java
@@ -1,29 +1,27 @@
-  
 import java.util.Scanner;
 public class C {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int[] list = new int[n];
-        int box = 0;
-        int count = 0;
+        long[] list = new long[n];
+        long box = 0;
+        long count = 0;
 
         for (int i = 0; i < list.length; i++) {
-            list[i] = scan.nextInt();
-            box = list[0];
-            System.out.println(box);
+            list[i] = scan.nextLong();
             if(i != 0) {
                 if(box > list[i]) {
-                    System.out.println(box);
                     count = count + box - list[i];
-                    System.out.println(count);
                 } else {
                     box = list[i];
                 }
+            } else {
+                box = list[0];
             }
         }
         System.out.println(count);
 
         scan.close();
     }
+    
 }
--- Sample.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Sample {
-    public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-        System.out.println(x);
-        scan.close();
-    }
-}
