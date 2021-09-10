91fc40a63226513bb3833963d1f502a2f9a41c2b
YaeoSys <yaeok@revolveok.co.jp>
2021-09-10 09:35:35 +0900

    189_A解答

--- ABC/176~200/ABC189/A.java
+++ ABC/176~200/ABC189/A.java
@@ -4,11 +4,17 @@ public class A {
         Scanner scan = new Scanner(System.in);
         String word = scan.next();
         String[] list = word.split("");
+        String str = list[0];
         for (int i = 0; i < list.length; i++) {
-            String str = list[0];
             if (i != 0) {
-                if (str == list[i]) {
-                    
+                if (str.equals(list[i])) {
+                    str = list[i];
+                    if(i == list.length - 1) {
+                        System.out.println("Won");
+                    }
+                } else {
+                    System.out.println("Lost");
+                    break;
                 }
             }
         }
