19e5fe8f993dc2864eb207fef06a882722abe44d
YaeoSys <yaeok@revolveok.co.jp>
2021-09-14 16:21:35 +0900

    追加

---
+++ ABC/201~225/ABC205/B.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class B {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+ 
+        int a = scan.nextInt();
+        String ans = "Yes";
+        int[] b = new int[a];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = scan.nextInt();
+        }
+        scan.close();
+        Arrays.sort(b);
+ 
+        for (int i = 0; i < b.length; i++) {
+            if (!(b[i] == (i + 1))) {
+                ans = "No";
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
