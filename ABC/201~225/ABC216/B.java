c2355917828cc17126d7c114074401ecdecff3d3
YaeoSys <yaeok@revolveok.co.jp>
2021-09-22 13:26:27 +0900

    216B

--- ABC/201~225/ABC216/B.java
+++ ABC/201~225/ABC216/B.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 public class B {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         String[] list = new String[n];
+
         for (int i = 0; i < list.length; i++) {
-            list[i] = scan.nextLine();
+            String s = sc.next();
+            String t = sc.next();
+
+            list[i] = s + " " + t;
         }
-        scan.close();
+        String res = "No";
+        myLoop:
+        for (int i = 0; i < list.length - 1; i++) {
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[i].equals(list[j])) {
+                    res = "Yes";
+                    break myLoop;
+                }
+            }
+        }
+        System.out.println(res);
+        sc.close();
     }
 }
-
-
-// import java.util.Scanner;
-// public class A {
-//     public static void main(String[] args) throws Exception {
-//         Scanner sc = new Scanner(System.in);
-//         int i = sc.nextInt();
-//         String[] list = new String[i];
-//         for (int j = 0; j < 3; j++) {
-//             list[i] = sc.nextLine();
-//             System.out.println(list[i]);
-//         }
-//         sc.close();
-//     }
-// }
